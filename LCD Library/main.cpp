1548855bfaf2a11a514fb738d00e158ddabf6ae7
LukeDWaller99 <lukedwaller99@gmail.com>
2022-06-11 17:31:41 +0100

    Base functionality of LCD Driver

---
+++ LCD Library/main.cpp
@@ -0,0 +1,11 @@
+#include "mbed.h"
+#include "LCD.h"
+#include <cstdio>
+#include <cstring>
+// #include "string.h"
+
+// LCD LCD(PD_11, PD_12, PD_13, PD_0, PD_1, PD_2, PD_3, PD_4, PD_5, PD_6, PD_7);
+LCD LCD(PD_11, PD_12, PD_13, PD_4, PD_5, PD_6, PD_7);
+
+int main(){
+}
