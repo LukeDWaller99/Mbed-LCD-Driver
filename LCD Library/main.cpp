6aa0bc18a7fabf27a63b22e908ba7811c9d610c8
Luke Waller <57799886+LukeDWaller99@users.noreply.github.com>
2022-06-11 17:32:21 +0100

    Update main.cpp

--- LCD Library/main.cpp
+++ LCD Library/main.cpp
@@ -4,8 +4,8 @@
 #include <cstring>
 // #include "string.h"
 
-// LCD LCD(PD_11, PD_12, PD_13, PD_0, PD_1, PD_2, PD_3, PD_4, PD_5, PD_6, PD_7);
-LCD LCD(PD_11, PD_12, PD_13, PD_4, PD_5, PD_6, PD_7);
+// LCD LCD(PD_11, PD_12, PD_13, PD_0, PD_1, PD_2, PD_3, PD_4, PD_5, PD_6, PD_7); // 8-bit 
+LCD LCD(PD_11, PD_12, PD_13, PD_4, PD_5, PD_6, PD_7); // 4-bit
 
 int main(){
 }
